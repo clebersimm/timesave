39ea5090322749cbd40d39423c84d83a154770f1
Cleber Simm <clebersimm@gmail.com>
2025-05-15 10:01:27 -0300

    feat: color for the boxes
    issue #3
    
    Green for Credit
    Red for debit
    And the total depends on the total, if negative(debit) red,  positive green(credit) and black zero

--- components/ui/CreditInfo.tsx
+++ components/ui/CreditInfo.tsx
@@ -4,20 +4,22 @@ import { useTaskContext } from "@/src/context/TaskContext";
 
 export default function CreditInfo() {
   const { totalCredit, totalDebit } = useTaskContext();
+  const total = totalCredit - totalDebit;
+  const totalColor = total > 0 ? style.boxColorCredit : total < 0 ? style.boxColorDebit : style.boxColorZero; // Green for positive, red for negative, black for zero
 
   return (
     <View style={style.container}>
-      <View style={style.box}>
+      <View style={[style.box, style.boxColorCredit]}>
         <Text style={{ fontWeight: "bold", fontSize: 18 }}>Crédito</Text>
         <Text>{totalCredit}</Text>
       </View>
-      <View style={style.box}>
+      <View style={[style.box, style.boxColorDebit]}>
         <Text style={{ fontWeight: "bold", fontSize: 18 }}>Débitos</Text>
         <Text>{totalDebit}</Text>
       </View>
-      <View style={style.box}>
+      <View style={[style.box, totalColor]}>
         <Text style={{ fontWeight: "bold", fontSize: 18 }}>Total</Text>
-        <Text>{totalCredit - totalDebit}</Text>
+        <Text>{total}</Text>
       </View>
     </View>
   );
@@ -37,4 +39,14 @@ const style = StyleSheet.create({
     borderRadius: 5,
     backgroundColor: "#f9f9f9", // A variant of white
   },
+  boxColorDebit: {
+    borderColor: "#ff0000", // Red for debit
+  },
+  boxColorCredit: {
+    borderColor: "#00ff00", // Green for credit
+  },
+  boxColorZero: {
+    borderColor: "#000000", // Black for zero
+  }
+
 });
