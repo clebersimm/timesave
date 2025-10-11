4d90bc8c772b066a90d861e5102243e4ec65a6be
Cleber Simm <clebersimm@gmail.com>
2025-10-11 10:17:56 -0300

    refactor: enhance HistoryCard component to improve value display and styling

--- components/historic/HistoryCard.tsx
+++ components/historic/HistoryCard.tsx
@@ -1,18 +1,28 @@
 import { TaskOutput } from "@/src/services/TaskService";
 import OperationEnum from "@/src/shared/OperationEnum";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Card, Text } from "react-native-paper";
+import TaskTypeIcon from "../TodoTable/TaskTypeIcon";
 
 export interface HistoryCardProps {
     task: TaskOutput;
 }
 
 export function HistoryCard({ task }: HistoryCardProps) {
-    function formatValue(): string{
-        if (task.operation === OperationEnum.DEBIT){
-            return `-${task.value}`;
+    function formatValue(): React.ReactNode {
+        let value = <Text style={style.labelCredit}>Credit: {`${task.value}`}</Text>;
+        if (task.operation === OperationEnum.DEBIT) {
+            value = <Text style={style.labelDebit}>Debit: {`-${task.value}`}</Text>;
         }
-        return `${task.value}`;
+        return (
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8, marginTop: 8 }}>
+                {taskTypeIcon()}
+                {value}
+            </View>
+        );
+    }
+    function taskTypeIcon(): React.ReactNode {
+        return <TaskTypeIcon type={task.type} />;
     }
     return (
         <Card>
@@ -25,13 +35,18 @@ export function HistoryCard({ task }: HistoryCardProps) {
                     <Text variant="bodyMedium">Created: {task.createdAt}</Text>
                     <Text variant="bodyMedium">Finished: {task.updatedAt}</Text>
                 </View>
-                <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-                    <Text variant="bodyMedium">Operation: {task.operation}</Text>
-                    <Text variant="bodyMedium">Value: {formatValue()}</Text>
-                </View>
-                <Text variant="bodyMedium">Type: {task.type}</Text>
                 <Text variant="bodyMedium">Tags: {task.tags}</Text>
+                {formatValue()}
             </Card.Content>
         </Card>
     );
 }
+
+const style = StyleSheet.create({
+    labelCredit: {
+        color: "#008000", // Dark green for credit
+    },
+    labelDebit: {
+        color: "#ff0000", // Red for debit
+    },
+});
